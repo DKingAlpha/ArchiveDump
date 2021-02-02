905d84a8ae40695b3e3021ec8643ff0d8b82dce2
DKing <dkingchn@gmail.com>
2021-02-02 19:33:25 +0800

    wip sync

--- ArchiveDump/ArchiveDump.cpp
+++ ArchiveDump/ArchiveDump.cpp
@@ -7,31 +7,16 @@
 #include <fstream>
 
 #include "RADR.hpp"
+#include "RADR.hpp"
 #include "CR2W.hpp"
 
 #include <Windows.h>
 
 using namespace std;
 
-int main(int argc, const char** argv)
+int extract_radr_archive(filesystem::path filepath, filesystem::path dump_path)
 {
-    OodleHelper::Initialize();
-
-    if (argc <= 1) {
-        printf("Usage:\n"
-                "    %S InputFile [OutputDir]\n\n"
-                "    * default value of OutputDir is filename\n", filesystem::path(argv[0]).filename().c_str());
-        return 1;
-    }
-    filesystem::path filepath = argv[1];
-    filesystem::path dump_path = filepath.stem();
-    if (argc >= 3) { dump_path = argv[2]; }
-
-    //for (const auto& fp : filesystem::directory_iterator("G:\\Games\\Cyberpunk 2077\\archive\\pc\\content"))
-    {
-        //const filesystem::path& filepath = fp.path();
-        
-        const wstring map_name = L"RDAR_" + wstring(filepath.filename().c_str());
+        const wstring map_name = L"RDAR_" + wstring(filepath.stem().c_str());
 
         const auto filesize = filesystem::file_size(filepath);
         const auto low_filesize = uint32_t(filesize & UINT32_MAX);
@@ -46,57 +31,138 @@ int main(int argc, const char** argv)
         auto hMapFile = CreateFileMapping(file_handle, NULL, PAGE_READONLY, high_filesize, low_filesize, map_name.c_str());
         if (hMapFile == NULL)
         {
-             printf("Could not create file mapping object: %d\n", GetLastError());
+            printf("Could not create file mapping object: %d\n", GetLastError());
             CloseHandle(file_handle);
             return 1;
         }
         auto file_content = MapViewOfFile(hMapFile, FILE_MAP_READ, 0, 0, filesize);
         if (file_content == NULL)
         {
-             printf("Could not map view of file: %d\n", GetLastError());
+            printf("Could not map view of file: %d\n", GetLastError());
             CloseHandle(hMapFile);
             CloseHandle(file_handle);
             return 1;
         }
         
-        printf("%S\n", filepath.c_str());
-
+        
+        filesystem::create_directories(dump_path);
+        
         RedArchive archive(file_content);
-        map<uint64_t, RedArchiveFile> filemap;
+        printf("========== RADR Archive ID: %S ==========\n", filepath.stem().c_str());
         for (uint32_t i = 0; i < archive.fileTable->fileEntryCount; i++)
         {
-            auto f = archive.GetFile(i);
-            if (!f.compressed) continue;
-            filemap[f.entry.id] = f;
-        }
+            RedArchiveFile f = archive.GetFile(i);
 
-        filesystem::create_directories(dump_path);
-
-        for (auto&& f : filemap)
-        {
-            if (!f.second.compressed) continue;
             {
-                auto savepath = dump_path / filesystem::path(to_string(f.first));
+                auto savepath = dump_path / filesystem::path(to_string(f.entry.id));
                 ofstream of(savepath.string());
-                of.write(reinterpret_cast<const char*>(f.second.data.data()), f.second.data.size());
+                of.write(reinterpret_cast<const char*>(f.data.data()), f.data.size());
             }
+            
+            printf("--------- Extract %s : %llu ---------\n", (f.compressed ? "compressed  " : "uncompressed"), f.entry.id);
 
-            // -----------  WIP  --------------
+            if (!f.compressed)
+                continue;
 
-            // unpack CR2W files
-            auto cr2w = f.second.Get<CR2W>();
-            assert(cr2w->header.magic == 'W2RC'); // CR2W
-            // printf("--------- %llu  ---------\n", f.first);
-            // for (auto&& ent : cr2w->entries<CR2WName>(1))
+            uint32_t magic = *reinterpret_cast<uint32_t*>(f.data.data());
+            if (magic == 'W2RC') // CR2W
             {
-                // printf("[Name] %d, %x\n", ent.value, ent.hash);
+                // -----------  WIP  --------------
+                // unpack CR2W files
+                auto cr2w = f.Get<CR2W>();
+                assert(cr2w->header.magic == 'W2RC'); // CR2W
+                #define ENT_OFFSET  ( reinterpret_cast<uintptr_t>(&ent) - reinterpret_cast<uintptr_t>(cr2w))
+                for (auto&& ent : cr2w->entries<CR2WName>())
+                {
+                    printf("[Name] %p %s, %x\n", ENT_OFFSET , ent.GetName(cr2w), ent.hash);
+                }
+
+                for (auto&& ent : cr2w->entries<CR2WImport>())
+                {
+                    printf("[Import] %p className: %s, depotPath: %s, flags: %x\n", 
+                        ENT_OFFSET,
+                        ent.GetTypeName(cr2w),
+                        ent.GetDepotPath(cr2w),
+                        ent.flags);
+                    
+                }
+
+                for (auto&& ent : cr2w->entries<CR2WProperty>())
+                {
+                    printf("[Property] %p className: %s, propertyName: %s\n", ENT_OFFSET, ent.GetTypeName(cr2w), ent.GetPropertyName(cr2w));
+                }
+
+                for (auto&& ent : cr2w->entries<CR2WExport>())
+                {
+                    printf("[Export]: %p %s\n", ENT_OFFSET, ent.GetName(cr2w).c_str());
+                    auto parent = ent.GetParent(cr2w);
+                    if (parent)
+                        printf("    [Parent]: %s\n", parent->GetName(cr2w).c_str());
+                    for (auto&& child : ent.GetChildren(cr2w))
+                    {
+                        printf("    [Child]: %s\n", child->GetName(cr2w).c_str());
+                    }
+                }
+
+                for (auto&& ent : cr2w->entries<CR2WBuffer>())
+                {
+
+                }
             }
+            else {
+                assert(0);
+            }
+
         }
 
         UnmapViewOfFile(file_content);
         CloseHandle(hMapFile);
         CloseHandle(file_handle);
+        return 0;
+}
 
+int main(int argc, const char** argv)
+{
+    OodleHelper::Initialize();
+
+    if (argc <= 1) {
+        printf("Usage:\n"
+                "    %S InputFileOrDir [OutputDir]\n\n"
+                "    * default value of OutputDir is filename\n", filesystem::path(argv[0]).filename().c_str());
+        return 1;
+    }
+
+    filesystem::path filepath = argv[1];
+    if (!filesystem::exists(filepath))
+    {
+        printf("File dor dir not exists\n");
+        return 1;
+    }
+
+    if (filesystem::is_regular_file(filepath))
+    {
+        filesystem::path default_dump_path = filepath.stem();
+        filesystem::path dump_path;
+        if (argc >= 3)
+            dump_path = argv[2];
+        else
+            dump_path = default_dump_path;
+        return extract_radr_archive(filepath, dump_path);
+    }
+
+    for (const auto& fp : filesystem::directory_iterator(filepath))
+    {
+        filesystem::path default_dump_path = "dump";
+        filesystem::path dump_path;
+        if (argc >= 3)
+            dump_path = argv[2];
+        else
+            dump_path = default_dump_path;
+        const filesystem::path& fpath = fp.path();
+        filesystem::path dpath = dump_path / filepath.stem();
+        if (fpath.extension() != "archive")
+            continue;
+        extract_radr_archive(fpath, dpath);
     }
     return 0;
 }
--- ArchiveDump/CR2W.hpp
+++ ArchiveDump/CR2W.hpp
@@ -1,7 +1,28 @@
+#pragma once
+
 #include <stdint.h>
+#include <string>
+#include <vector>
 
 // ---- CREDIT ----
 // WolvenKit: for CR2W structure
+
+
+
+// --------------------
+template<typename T>
+class IteratableStructs {
+public:
+    IteratableStructs(T* p, uint32_t count) : pstart(p), pend(p + count + 1) {}
+    IteratableStructs() : pstart(nullptr), pend(nullptr) {}
+    T* begin() const { return pstart; }
+    T* end() const { return pend; };
+private:
+    T* pstart = nullptr;
+    T* pend = nullptr;
+};
+
+// --------------------
 
 #pragma pack(push, CR2W, 1)
 
@@ -17,59 +38,187 @@ struct CR2WHeader {
     uint32_t numChunks;
 };
 
-struct CR2WTable {
+struct CR2WTableIndex {
     uint32_t pos;
-    uint32_t count;
+    uint32_t max_index;     // 1-based indexing
     uint32_t crc32;
 };
 
+// ---- forward declaration ----
+struct CR2WStrings;
+struct CR2WName;
+struct CR2WImport;
+struct CR2WProperty;
+struct CR2WExport;
+struct CR2WBuffer;
+struct CR2WEmbedded;
+
+struct CR2W {
+    CR2WHeader header;
+    CR2WTableIndex  tables[10];
+
+public:
+    template<typename T>
+    T* Get(intptr_t offset, void* rel = nullptr)
+    {
+        if (rel == nullptr)
+            rel = this;
+        return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(rel) + offset);
+    }
+
+    template<typename T>
+    T* GetTableEntry(int table_index, int entry_index) { return Get<T>(tables[table_index].pos) + entry_index; }
+
+    template<typename T>
+    constexpr int get_index_of_table()
+    {
+        constexpr int index = 
+            (std::is_same<T, CR2WStrings>::value)  ? 0 :
+            (std::is_same<T, CR2WName>::value)     ? 1 :
+            (std::is_same<T, CR2WImport>::value)   ? 2 :
+            (std::is_same<T, CR2WProperty>::value) ? 3 :
+            (std::is_same<T, CR2WExport>::value)   ? 4 :
+            (std::is_same<T, CR2WBuffer>::value)   ? 5 :
+            (std::is_same<T, CR2WEmbedded>::value) ? 6 :
+            -1;
+        static_assert(index != -1);
+        return index;
+    }
+
+    template<typename T>
+    IteratableStructs<T> entries()
+    {
+        int table_index = get_index_of_table<T>();
+        uint32_t table_pos = tables[table_index].pos;
+        uint32_t table_ent_count = tables[table_index].max_index - 1;
+        if (table_pos && table_ent_count > 0)
+            return IteratableStructs<T> { Get<T>(table_pos), table_ent_count};
+        else
+            return IteratableStructs<T> {};
+    }
+
+    template<typename T>
+    T* entry(uint32_t index=0)
+    {
+        int table_index = get_index_of_table<T>();
+        uint32_t table_pos = tables[table_index].pos;
+        uint32_t table_ent_count = tables[table_index].max_index - 1;
+        if (table_pos && table_ent_count > 0 && index < table_ent_count)
+            return Get<T>(table_pos)+ index;
+        else
+            return nullptr;
+    }
+
+};
+
 // --------------------
+struct CR2WStrings {
+    // char strings[0];       // sequential strings splited by null byte
+    const char* const GetString(int offset)
+    {
+        return reinterpret_cast<char*>(this) + offset;
+    }
+};
+
 struct CR2WName {
     uint32_t value;
     uint32_t hash;
+public:
+    const char* const GetName(CR2W* ctx) { return ctx->entry<CR2WStrings>()->GetString(value); }
 };
 
 struct CR2WImport {
     uint32_t depotPath;
     uint16_t className;
     uint16_t flags;
-};
 
-// --------------------
-template<typename T>
-class EntryIteratable {
-public:
-    EntryIteratable(T* p, int count): pstart(p), pend(p+count+1) {}
-    T* begin() const { return pstart; }
-    T* end() const { return pend; };
-private:
-    T* pstart = nullptr;
-    T* pend = nullptr;
-};
-
-struct CR2W {
-    CR2WHeader header;
-    CR2WTable  tables[10];
-
-public:
-    template<typename T>
-    T* Get(intptr_t offset, void* rel=nullptr)
+    const char* const GetTypeName(CR2W* ctx)  { return ctx->entry<CR2WName>(className)->GetName(ctx); }
+    const char* const GetDepotPath(CR2W* ctx) { return ctx->entry<CR2WStrings>()->GetString(depotPath); }
+    int64_t GetIndex(CR2W* ctx)
     {
-        if (rel == nullptr)
-            rel = this;
-        return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(rel) + offset);
+        int64_t self_index = this - ctx->entry<CR2WImport>();
+        assert(self_index >= 0);
+        return self_index;
     }
+};
+
+struct CR2WProperty {
+    uint16_t className;
+    uint16_t classFlags;
+    uint16_t propertyName;
+    uint16_t propertyFlags;
+    uint64_t hash;
+
+    const char* const GetTypeName(CR2W* ctx) { return ctx->entry<CR2WName>(className)->GetName(ctx); }
+    const char* const GetPropertyName(CR2W* ctx) { return ctx->entry<CR2WStrings>()->GetString(propertyName); }
+};
+
+struct CR2WExport {
+    uint16_t className;
+    uint16_t objectFlags;   // 0x2000: cooked
+    uint32_t parentID;      // parentID=1: first CR2WExport, parentID=0: no parent
+    uint32_t dataSize;
+    uint32_t dataOffset;
+    uint32_t tpl;           // template
+    uint32_t crc32;
     
-    template<typename T>
-    T* GetTableEntry(int table_index, int entry_index) { return Get<T>(tables[table_index].pos) + entry_index; }
+    const char* const GetTypeName(CR2W* ctx) { return ctx->entry<CR2WName>(className)->GetName(ctx); }
+    std::string GetName(CR2W* ctx) { return std::string(GetTypeName(ctx)) + "#" + std::to_string(GetIndex(ctx)); }
 
-    char* GetStringAtOffset(int offset) { return Get<char>(tables[0].pos) + offset; }
-
-    template<typename T>
-    EntryIteratable<T> entries(int index)
+    int64_t GetIndex(CR2W* ctx)
     {
-        return EntryIteratable<T>(Get<T>(tables[index].pos), tables[index].count);
+        int64_t self_index = this - ctx->entry<CR2WExport>();
+        assert(self_index >= 0);
+        return self_index;
+    }
+
+    CR2WExport* GetParent(CR2W* ctx)
+    {
+        if (parentID)
+            return ctx->entry<CR2WExport>(parentID - 1);
+        else
+            return nullptr;
+    }
+
+    std::vector<CR2WExport*> GetChildren(CR2W* ctx)
+    {
+        std::vector<CR2WExport*> children;
+        int64_t self_index = this - ctx->entry<CR2WExport>();
+        assert(self_index >= 0);
+        for (auto&& ent : ctx->entries<CR2WExport>())
+        {
+            if (ent.parentID - 1 == self_index)
+                children.push_back(&ent);
+        }
+        return children;
     }
 };
+
+struct CR2WBuffer {
+    uint32_t flags;
+    uint32_t index;
+    uint32_t offset;
+    uint32_t dataSize;
+    uint32_t memSize;
+    uint32_t crc32;
+};
+
+struct CR2WEmbedded {
+    uint32_t importIndex;   // 1: first CR2WImport, 0: no import
+    uint32_t path;
+    uint32_t pathHash;
+    uint32_t dataOffset;
+    uint32_t dataSize;
+
+    CR2WImport* GetImport(CR2W* ctx)
+    {
+        if (importIndex)
+            return ctx->entry<CR2WImport>(importIndex - 1);
+        else
+            return nullptr;
+    }
+    const char* const GetPath(CR2W* ctx) { return ctx->entry<CR2WStrings>()->GetString(path); }
+};
+
 
 #pragma pack(pop, CR2W)
